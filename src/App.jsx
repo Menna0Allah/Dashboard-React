e8f518dc5e035b4e2652cc247e9df52f1f34512c
menna <menna.allah04@gmail.com>
2025-03-27 19:48:42 +0200

    Finishing the setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Dashboard from './components/dashboard'
+import './App.css'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'; 
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 botton-4" style={{zIndex:'1000'}}>
+            <TooltipComponent content="Settings" position='Top'>
+              <button>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
