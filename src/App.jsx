bffb2afe8bb2a4ebd639b3b76b9d0d078566aad9
menna <menna.allah04@gmail.com>
2025-03-30 04:41:44 +0200

    Add Syncfusion dependencies and implement license registration in main component

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,48 @@ import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'; 
 
 function App() {
+
+  const activeMenu = true;
+
   return (
     <div>
       <BrowserRouter>
+
         <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 botton-4" style={{zIndex:'1000'}}>
+          
+          <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
+
             <TooltipComponent content="Settings" position='Top'>
-              <button>
+              
+              <button type="button" className='bg-blue-900 text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+              style={{
+                borderRadius: '50%'
+              }}>
                 <FiSettings />
               </button>
+
             </TooltipComponent>
+
           </div>
+
+          {activeMenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              sidebar 
+            </div>
+          ): (
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              SideBar
+            </div>
+          )}
+
+          <div className={
+            `dark:bg-main-bg bg-main-bg min-h-screen w-full
+            ${activeMenu ? 'md:ml-72'
+            : ' flex-2'}`
+          }>
+
+          </div>
+
         </div>
       </BrowserRouter>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+// For React specific packages
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXxceXVcRGNYVUByXUZWYUA=');
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
