b105d6090d1e76a25f9073ee7ea17ef692352ae8
Menna Allah <menna.allah04@gmail.com>
2025-04-13 22:00:37 +0200

    Add new state for the click and function to toggle it everytime clicked; in Navbar component call them to handle it with the page

--- src/contexts/contextProvider.jsx
+++ src/contexts/contextProvider.jsx
@@ -11,6 +11,11 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(false);
+    const [isClicked, setIsClicked] = useState(initialState);
+
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState, [clicked]:true });
+    }
 
 
     return (
@@ -19,6 +24,9 @@ export const ContextProvider = ({ children }) => {
                 // activeMenu: activeMenu,
                 activeMenu,
                 setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick
             }}
         >
             {children}
